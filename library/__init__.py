78d7c182bc2f7f08a3244654dec6f3eaa48befa0
Venax <81310818+venaxyt@users.noreply.github.com>
2021-07-11 01:04:07 +0200

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -1,1 +1,11 @@
+# Module made by @venaxyt on Github
+import os
 
+def purple(text):
+    os.system("")
+    number = 40
+    faded = ""
+    for line in text.splitlines():
+        faded += (f"\033[38;2;{number};0;220m {line}\n")
+        number += 15
+    return faded
