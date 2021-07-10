d677928e5fb5a7aa28f78bfda11dfd6dc72f18b3
Venax <81310818+venaxyt@users.noreply.github.com>
2021-07-11 01:18:00 +0200

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -9,3 +9,12 @@ def purple(text):
         faded += (f"\033[38;2;{number};0;220m {line}\n")
         number += 15
     return faded
+
+def water(text):
+    os.system("")
+    number = 10
+    faded = ""
+    for line in text.splitlines():
+        faded += (f"\033[38;2;0;{number};255m {line}\n")
+        number += 15
+    return faded
