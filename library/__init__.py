880da4d119aadaa3277f9de060b2b74e70aa37e3
Venax <81310818+venaxyt@users.noreply.github.com>
2021-07-11 01:31:16 +0200

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -18,3 +18,16 @@ def water(text):
         faded += (f"\033[38;2;0;{number};255m {line}\n")
         number += 15
     return faded
+
+def fire(text):
+    os.system("")
+    number = 150
+    number2 = 0
+    faded = ""
+    for line in text.splitlines():
+        faded += (f"\033[38;2;255;{number};{number2}m {line}\n")
+        if not number < 0:
+            number -= 23
+        else:
+            number2 += 23
+    return faded
