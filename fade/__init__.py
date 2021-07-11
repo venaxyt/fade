4394a581e9355dc481ebb31e77425f4f5c5ee5f2
Venax <81310818+venaxyt@users.noreply.github.com>
2021-07-11 19:03:13 +0200

    Update __init__.py

--- fade/__init__.py
+++ fade/__init__.py
@@ -1,69 +1,81 @@
 # Module made by @venaxyt on Github
-import os, random
+from random import randint
+from os import system
 
 def purplepink(text):
-    os.system("")
+    system("")
     number = 40
     faded = ""
     for line in text.splitlines():
         faded += (f"\033[38;2;{number};0;220m{line}\033[0m\n")
-        number += 15
+        if not number == 255:
+            number += 15
+            if number > 255:
+                number = 255
     return faded
 
 def greenblue(text):
-    os.system("")
+    system("")
     number = 100
     faded = ""
     for line in text.splitlines():
         faded += (f"\033[38;2;0;255;{number}m{line}\033[0m\n")
-        number += 15
+        if not number == 255:
+            number += 15
+            if number > 255:
+                number = 255
     return faded
 
 def pinkred(text):
-    os.system("")
+    system("")
     number = 255
     faded = ""
     for line in text.splitlines():
         faded += (f"\033[38;2;255;0;{number}m{line}\033[0m\n")
-        number -= 20
+        if not number == 0:
+            number -= 20
+            if number < 0:
+                number = 0
     return faded
 
 def purpleblue(text):
-    os.system("")
+    system("")
     number = 110
     faded = ""
     for line in text.splitlines():
         faded += (f"\033[38;2;{number};0;255m{line}\033[0m\n")
-        if not number < 0:
+        if not number == 0:
             number -= 15
             if number < 0:
                 number = 0
     return faded
 
 def water(text):
-    os.system("")
+    system("")
     number = 10
     faded = ""
     for line in text.splitlines():
         faded += (f"\033[38;2;0;{number};255m{line}\033[0m\n")
-        number += 15
+        if not number == 255:
+            number += 15
+            if number > 255:
+                number = 255
     return faded
 
 def fire(text):
-    os.system("")
-    number = 150
-    number2 = 0
+    system("")
+    number = 250
     faded = ""
     for line in text.splitlines():
-        faded += (f"\033[38;2;255;{number};{number2}m{line}\n")
-        if not number < 0:
-            number -= 23
-        else:
-            number2 += 23
+        faded += (f"\033[38;2;255;{number};0m{line}\033[0m\n")
+        if not number == 0:
+            number -= 20
+            if number < 0:
+                number = 0
     return faded
 
 def brazil(text):
-    os.system("")
+    system("")
     number = 0
     faded = ""
     for line in text.splitlines():
@@ -73,10 +85,10 @@ def brazil(text):
     return faded
 
 def random(text):
-    os.system("")
+    system("")
     faded = ""
     for line in text.splitlines():
         for character in line:
-            faded += (f"\033[38;2;{random.randint(0,255)};{random.randint(0,255)};{random.randint(0,255)}m{character}\033[0m")
+            faded += (f"\033[38;2;{randint(0,255)};{randint(0,255)};{randint(0,255)}m{character}\033[0m")
         faded += "\n"
     return faded
